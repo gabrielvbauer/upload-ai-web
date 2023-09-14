d92367f87c29b880c6e97ca96a0b694dfcdeb846
gabrielvbauer <gabriel.bauer9@hotmail.com>
2023-09-14 15:50:29 -0300

    feat: integration done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FileVideo, Github, UploadIcon, Wand2 } from "lucide-react";
+import { Wand2 } from "lucide-react";
 
 import { Button } from "./components/ui/button";
 import { Separator } from "./components/ui/separator";
@@ -12,26 +12,37 @@ import {
   SelectValue,
 } from "./components/ui/select";
 import { Slider } from "./components/ui/slider";
+import { VideoInputForm } from "./components/video-input-form";
+import { Header } from "./components/header";
+import { PromptSelect } from "./components/prompt-select";
+import { useState } from "react";
+import { useCompletion } from "ai/react";
 
 export function App() {
+  const [temperature, setTemperature] = useState(0.5);
+  const [videoId, setVideoId] = useState<string | null>(null);
+
+  const {
+    input,
+    setInput,
+    handleInputChange,
+    handleSubmit,
+    completion,
+    isLoading,
+  } = useCompletion({
+    api: "http://localhost:3333/ai/complete",
+    body: {
+      videoId,
+      temperature,
+    },
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
   return (
     <div className="min-h-screen flex flex-col">
-      <div className="px-6 py-3 flex items-center justify-between border-b">
-        <h1 className="text-xl font-bold">upload.ai</h1>
-
-        <div className="flex items-center gap-3">
-          <span className="text-sm text-muted-foreground">
-            Desenvolvido com 💜 na NLW da Rocketseat
-          </span>
-
-          <Separator orientation="vertical" className="h-6" />
-
-          <Button variant="outline">
-            <Github className="w-4 h-4 mr-2" />
-            Github
-          </Button>
-        </div>
-      </div>
+      <Header />
 
       <main className="flex-1 p-6 flex gap-6">
         <div className="flex flex-col flex-1 gap-4">
@@ -39,11 +50,14 @@ export function App() {
             <Textarea
               className="resize-none p-4 leading-relaxed"
               placeholder="Inclua o prompt para a IA..."
+              value={input}
+              onChange={handleInputChange}
             />
             <Textarea
               className="resize-none p-4 leading-relaxed"
               placeholder="Resultado gerado pela IA..."
               readOnly
+              value={completion}
             />
           </div>
 
@@ -55,55 +69,14 @@ export function App() {
         </div>
 
         <aside className="w-80 space-y-6">
-          <form className="space-y-6">
-            <label
-              htmlFor="video"
-              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
-            >
-              <FileVideo className="w-4 h-4" />
-              Selecione um vídeo
-            </label>
-
-            <input
-              type="file"
-              id="video"
-              accept="video/mp4"
-              className="sr-only"
-            />
-
-            <Separator />
-
-            <div className="space-y-2">
-              <Label htmlFor="transcription_prompt">Prompt de transição</Label>
-              <Textarea
-                id="transcription_prompt"
-                className="h-20 resize-none leading-relaxed"
-                placeholder="Inclua palavras-chave mencionadas no vídeo separadas por (,)"
-              />
-            </div>
-
-            <Button type="submit" className="w-full">
-              Carregar vídeo
-              <UploadIcon className="w-4 h-4 ml-2" />
-            </Button>
-          </form>
+          <VideoInputForm onVideoUploaded={setVideoId} />
 
           <Separator />
 
-          <form className="space-y-6">
+          <form onSubmit={handleSubmit} className="space-y-6">
             <div className="space-y-2">
               <Label>Prompt</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Selecione um prompt..." />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="title">Título do YouTube</SelectItem>
-                  <SelectItem value="description">
-                    Descrição do YouTube
-                  </SelectItem>
-                </SelectContent>
-              </Select>
+              <PromptSelect onPromptSelected={setInput} />
             </div>
 
             <div className="space-y-2">
@@ -125,7 +98,13 @@ export function App() {
 
             <div className="space-y-4">
               <Label>Temperatura</Label>
-              <Slider min={0} max={1} step={0.1} />
+              <Slider
+                min={0}
+                max={1}
+                step={0.1}
+                value={[temperature]}
+                onValueChange={(value) => setTemperature(value[0])}
+              />
               <span className="block text-xs text-muted-foreground italic leading-relaxed">
                 Valores mais altos tendem a deixar o resultado mais criativo e
                 com possíveis erros.
@@ -134,7 +113,7 @@ export function App() {
 
             <Separator />
 
-            <Button type="submit" className="w-full">
+            <Button type="submit" disabled={isLoading} className="w-full">
               Executar <Wand2 className="w-4 h-4 ml-2" />
             </Button>
           </form>
---
+++ src/components/prompt-select.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { api } from "@/lib/axios";
+
+interface Prompt {
+  id: string;
+  title: string;
+  template: string;
+}
+
+interface PromptSelectProps {
+  onPromptSelected: (template: string) => void;
+}
+
+export function PromptSelect({ onPromptSelected }: PromptSelectProps) {
+  const [prompts, setPrompts] = useState<Prompt[] | null>(null);
+
+  async function fetchPrompts() {
+    const response = await api.get("/prompts");
+    setPrompts(response.data);
+  }
+
+  function handlePromptSelected(promptId: string) {
+    const selectedPrompt = prompts?.find((prompt) => prompt.id === promptId);
+
+    if (!selectedPrompt) {
+      return;
+    }
+
+    onPromptSelected(selectedPrompt.template);
+  }
+
+  useEffect(() => {
+    fetchPrompts();
+  }, []);
+
+  return (
+    <Select onValueChange={handlePromptSelected}>
+      <SelectTrigger>
+        <SelectValue placeholder="Selecione um prompt..." />
+      </SelectTrigger>
+      <SelectContent>
+        {prompts?.map((prompt) => (
+          <SelectItem key={prompt.id} value={prompt.id}>
+            {prompt.title}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}
